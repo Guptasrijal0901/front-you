d2e0aecbece185ed6def8c96adb6d186a4504d66
srijal gupta <guptasrijal0901@gmail.com>
2023-10-05 08:15:07 +0530

    msg

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Navbar from './Components/Navbar';
+import './App.css';
+const App = () => {
+  return (
+    <div className='App'>
+          <Navbar/>
+    </div>
+  )
+}
+export default App;
