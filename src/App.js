63f97777bca8c2d196685aab921cd1f3da0e18d1
srijal gupta <guptasrijal0901@gmail.com>
2023-10-06 23:24:28 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import Navbar from './Components/Navbar';
+// import Navbar from './Components/Navbar';
+import Create from "./Components/Create";
+import Read from "./Components/Read";
+import Update from "./Components/Update";
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const App = () => {
   return (
     <div className='App'>
-          <Navbar/>
+    <BrowserRouter>
+      <Routes>
+      <Route exact path='/create' element= {<Create/>}/>
+      <Route exact path='/all' element= {<Read/>}/>
+      <Route exact path='/update' element= {<Update/>}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   )
 }
